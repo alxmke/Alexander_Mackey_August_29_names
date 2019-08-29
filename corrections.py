8a8b3fab49c50bc1dd5fd686ef78e03e681c4373
Alex Mackey <alxmke@berkeley.edu>
2019-08-29 12:49:38 -0700

    added outline/description of corrections process

--- corrections.py
+++ corrections.py
@@ -1,3 +1,19 @@
+'''
+The basic premise of `corrections` will be to take in the user's given context C, and compare
+each given piece of text T against it to find which words will or won't be corrected. If a
+word W is determined to be a near-enough match to a word X within the context phrase P as to
+be corrected, it will correct W to X, and add the words left of X within P to the corrected
+string S. It will then search the rest of the text for the remaining words on the right side,
+moving forward a word in the phrase each time there is a miss, and moving forward a word in
+the text AND the phrase each time there's a hit, correcting the text word to the phrase word in S.
+
+For example, if in C, we have P = "President John F Kennedy", and T = "Jon Kenedy and friends",
+`corrections` aims to find Jon associated to P via John, and make C = "President John", and then
+it wouldn't find 'F', as the next word is 'Kenedy', and so it would move forward in P to 'Kennedy',
+and then find 'Kennedy' close enough to 'Kenedy' (and so replace it), and with no other context
+should then place 'and' and 'friends' making C = "President John F Kennedy and friends", with
+corrections [(+ President),(Jon -> John),(+ F),(Kenedy -> Kennedy)].
+'''
 from jaro import jaro
 from collections import defaultdict
 class Trie:
@@ -6,6 +22,7 @@ class Trie:
 			return defaultdict(_trie)
 		self.T = _trie()
 
+	# insert a word into the Trie
 	def insert(self, word):
 		T = self.T
 		for c in word:
@@ -14,6 +31,7 @@ class Trie:
 
 	# TODO: consider word is empty or none, though it never should be
 	# TODO: consider when there are no words in the Trie
+	# search for the nearest match in the Trie
 	def search(self, word):
 		#T = self.T
 		#i = 0
@@ -22,14 +40,22 @@ class Trie:
 		#while(
 		pass
 
+	# TODO: make a helpful representation of Trie
+	def __repr__(self):
+		return self.T
+
+	def __str__(self):
+		return str(self.T)
+		
+
 class Corrector:
 	def __init__(self, context): 
 		# each phrase in the context, split into  word-by-word lists
 		P = [p.split() for p in context]
 		# trie to build a nearest match for word, presuming it's not in table D
 		T = Trie()
-		# relational hashtable associating words
-		D = dict()
+		# relational hashtable associating words to phrases, a list will be searched in case of a conflict
+		D = dict(list)
 
 		# process in each phrase in the context
 		for p in P:
@@ -61,8 +87,11 @@ class Corrector:
 		self.trie = T
 		self.context = D
 
+	# returns list of corrections made, as well as the corrected string
 	def corrections(self, text):
 		context = self.context
+		corrected_string = ""
+		corrections = []
 		for k in context:
 			print(k)
 		print(self.trie)
