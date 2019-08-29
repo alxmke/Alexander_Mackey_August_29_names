5e86ea1fcde1961645abf10e15ae3d72b5c89fde
Alex Mackey <alxmke@berkeley.edu>
2019-08-29 11:47:34 -0700

    working on trie structure, for nearest-context-word-matching

--- corrections.py
+++ corrections.py
@@ -1,12 +1,33 @@
 from jaro import jaro
+from collections import defaultdict
+class Trie:
+	def __init__(self, words=None):
+		def _trie():
+			return defaultdict(_trie)
+		self.T = _trie()
+
+	def insert(self, word):
+		T = self.T
+		for c in word:
+			T = T[c]
+		T[None]
+
+	# TODO: consider word is empty or none, though it never should be
+	# TODO: consider when there are no words in the Trie
+	def search(self, word):
+		#T = self.T
+		#i = 0
+		
+		# use next(iter(dict)) to get a key out of the dict when there's no match
+		#while(
+		pass
 
 class Corrector:
 	def __init__(self, context): 
 		# each phrase in the context, split into  word-by-word lists
 		P = [p.split() for p in context]
 		# trie to build a nearest match for word, presuming it's not in table D
-		level = [None]*26
-		T = level[:]
+		T = Trie()
 		# relational hashtable associating words
 		D = dict()
 
@@ -14,8 +35,13 @@ class Corrector:
 		for p in P:
 			# associate each word in the phrase to the rest, and its index in the phrase
 			for i in range(len(p)):
-				# (word, phrase, index) tuples in dict
-				D[p[i]] = (p, i)
+				word = p[i]
+				# (word, phrase, index) tuples in table D
+				D[word] = (p, i)
+				# loading word into trie T
+				C = T.insert(word)
+				
+				
 		'''
 		for k in D:
 			s, l, r = D[k]
@@ -32,13 +58,14 @@ class Corrector:
 		if len("".split()):
 			print("TEST")
 		'''
-
+		self.trie = T
 		self.context = D
 
 	def corrections(self, text):
 		context = self.context
 		for k in context:
 			print(k)
+		print(self.trie)
 
 
 
